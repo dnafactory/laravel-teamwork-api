61b9d3a853174a5bd2d8f3410182d49590809b00
Domenico Ragusa <domenico.ragusa@dnafactory.it>
2021-02-16 02:56:57 +0100

    register new interfaces

--- TeamworkServiceProvider.php
+++ TeamworkServiceProvider.php
@@ -15,7 +15,7 @@ class TeamworkServiceProvider extends ServiceProvider
             ], 'config');
         }
 
-        $this->app['router']->post(config('teamwork.webhook_url'), WebhookController::class.'@index');
+        $this->app['router']->post(config('teamwork.webhook_url'), WebhookController::class . '@index');
     }
 
     public function register()
@@ -62,15 +62,59 @@ class TeamworkServiceProvider extends ServiceProvider
             return $rawInboxes->setBaseUrl($baseUrlDesk)->setToken($tokenDesk);
         });
 
-        $this->app->singleton(Endpoints\Router::class, function () {
-            return new Endpoints\Router();
+        $this->app->singleton(RawEndpoints\Desk\CustomFields::class, function () use ($baseUrlDesk, $tokenDesk) {
+            $rawCustomFields = new RawEndpoints\Desk\CustomFields(app(HttpClient::class));
+            return $rawCustomFields->setBaseUrl($baseUrlDesk)->setToken($tokenDesk);
         });
-        
+
+        $this->app->singleton(Endpoints\Router::class);
+
         $this->app->singleton(Endpoints\Desk\Tickets::class);
         $this->app->singleton(Endpoints\Desk\Timelogs::class);
         $this->app->singleton(Endpoints\Desk\Users::class);
         $this->app->singleton(Endpoints\Desk\Customers::class);
         $this->app->singleton(Endpoints\Desk\Companies::class);
         $this->app->singleton(Endpoints\Desk\Inboxes::class);
+        $this->app->singleton(Endpoints\Desk\CustomFields::class);
+        $this->app->singleton(Endpoints\Desk\CustomFieldOptions::class);
+        $this->app->singleton(Endpoints\Projects\Timelogs::class);
+
+        $this->app->make(Endpoints\Desk\Tickets::class);
+        $this->app->make(Endpoints\Desk\Timelogs::class);
+        $this->app->make(Endpoints\Desk\Users::class);
+        $this->app->make(Endpoints\Desk\Customers::class);
+        $this->app->make(Endpoints\Desk\Companies::class);
+        $this->app->make(Endpoints\Desk\Inboxes::class);
+        $this->app->make(Endpoints\Desk\CustomFields::class);
+        $this->app->make(Endpoints\Desk\CustomFieldOptions::class);
+
+
+        $baseUrlProjects = $baseUrl . '/projects/api';
+        $tokenProjects = config('teamwork.projects.token');
+        $routerProjects = new Endpoints\Router();
+
+        $this->app->singleton(RawEndpoints\Projects\Timelogs::class,
+            function () use ($baseUrlProjects, $tokenProjects) {
+                $rawTimelogs = new RawEndpoints\Projects\Timelogs(app(HttpClient::class));
+                return $rawTimelogs->setBaseUrl($baseUrlProjects)->setToken($tokenProjects);
+            });
+
+        $this->app->singleton(RawEndpoints\Projects\Tasks::class,
+            function () use ($baseUrlProjects, $tokenProjects) {
+                $rawTimelogs = new RawEndpoints\Projects\Tasks(app(HttpClient::class));
+                return $rawTimelogs->setBaseUrl($baseUrlProjects)->setToken($tokenProjects);
+            });
+
+        $this->app->singleton(Endpoints\Projects\Timelogs::class, function () use ($routerProjects) {
+            $rawTimelogs = app(RawEndpoints\Projects\Timelogs::class);
+            return new Endpoints\Projects\Timelogs($rawTimelogs, $routerProjects);
+        });
+
+        $this->app->singleton(Endpoints\Projects\Tasks::class, function () use ($routerProjects) {
+            $rawTasks = app(RawEndpoints\Projects\Tasks::class);
+            return new Endpoints\Projects\Tasks($rawTasks, $routerProjects);
+        });
+
+
     }
 }
